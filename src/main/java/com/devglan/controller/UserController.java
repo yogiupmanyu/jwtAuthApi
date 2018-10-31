6eac9827c1dd206555ec18fdc7d829ff79dc6677
Yogesh Sharma <yogeshs@egov.cdac.in>
2018-10-31 18:28:48 +0530

    JWTAuthApi

---
+++ src/main/java/com/devglan/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.devglan.controller;
+
+import com.devglan.model.User;
+import com.devglan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value="/user", method = RequestMethod.GET)
+    public List<User> listUser(){
+        return userService.findAll();
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public User getOne(@PathVariable(value = "id") Long id){
+        return userService.findById(id);
+    }
+
+}
---
+++ src/main/java/com/devglan/service/UserService.java
@@ -0,0 +1,15 @@
+package com.devglan.service;
+
+import com.devglan.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User save(User user);
+    List<User> findAll();
+    void delete(long id);
+    User findOne(String username);
+
+    User findById(Long id);
+}
